c6960a8d8897a29335fd65e3437ea6793950f7ef
ArC48 <beqa3352@gmail.com>
2024-04-12 22:31:04 +0400

    Initial Commit

---
+++ Beqa_project-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beqa_project-1
+//
+//  Created by beqa on 12.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
