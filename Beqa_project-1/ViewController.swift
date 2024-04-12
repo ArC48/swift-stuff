be197550a058e3cdf024e27b9db266e8fa17c3a4
ArC48 <beqa3352@gmail.com>
2024-04-12 23:22:16 +0400

    swift 1st project update

--- Beqa_project-1/ViewController.swift
+++ Beqa_project-1/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var logsText: UILabel!
+    var userLogs: String = "Logs:\n"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func onClickBlue(_ sender: UIButton) {
+        view.backgroundColor = .blue
+        logEvent(text: "Changed to Blue")
+    }
+    
 
+    @IBAction func onClickRed(_ sender: UIButton) {
+        view.backgroundColor = .red
+        logEvent(text: "Changed to Red")
+    }
+    @IBAction func onClickGreen(_ sender: UIButton) {
+        view.backgroundColor = .green
+        logEvent(text: "Changed to Green")
+    }
+    
+    private func logEvent(text:String) {
+        userLogs += text + "\n"
+        logsText.text = userLogs
+    }
 }
 
